8f9aa95302d02d9523aff72d8b320e23448b7a01
Kou Kinoshita <kouichi.but.the.ichi.is.silent@gmail.com>
2025-04-27 19:34:15 -0300

    Added register logic

--- meu-PFC/src/app/@pages/auth/login/login.component.ts
+++ meu-PFC/src/app/@pages/auth/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [ButtonModule,
-    CardModule
+    CardModule,
+    RouterModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
