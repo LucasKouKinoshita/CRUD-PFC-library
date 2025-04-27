7f2e0cd5624fbd3bec5bfbce2b9b66932ee57cfc
Kou Kinoshita <kouichi.but.the.ichi.is.silent@gmail.com>
2025-04-27 20:49:16 -0300

    Created change password method

--- meu-PFC/src/app/@services/auth.service.ts
+++ meu-PFC/src/app/@services/auth.service.ts
@@ -2,10 +2,12 @@ import { inject, Injectable, signal } from '@angular/core';
 import {
   Auth,
   createUserWithEmailAndPassword,
+  reauthenticateWithCredential,
+  updatePassword,
   updateProfile,
   user,
 } from '@angular/fire/auth';
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { EmailAuthProvider, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { from, Observable } from 'rxjs';
 import { UserInterface } from '../@interfaces/user.interface';
 
@@ -59,5 +61,27 @@ export class AuthService {
     const promise = signOut(this.firebaseAuth)
     this.currentUserSig.set(null);
     return from(promise)
+  }
+
+  changePassword(newPassword: string, currentPassword: string): Observable<void> {
+    const user = this.firebaseAuth.currentUser;
+
+    if (user) {
+      const credential = EmailAuthProvider.credential(user.email || '', currentPassword);
+
+      return from(
+        reauthenticateWithCredential(user, credential)
+          .then(() => {
+            return updatePassword(user, newPassword);
+          })
+          .catch((error) => {
+            throw error;
+          })
+      );
+    }
+
+    return new Observable<void>((observer) => {
+      observer.error('No user is signed in.');
+    });
   }
 }
