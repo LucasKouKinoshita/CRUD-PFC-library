7d4f34f889b9acc9e36218f05e2e16dec3634026
Kou Kinoshita <kouichi.but.the.ichi.is.silent@gmail.com>
2025-04-27 18:31:08 -0300

    Added firebase packages and created auth service with register method

---
+++ meu-PFC/src/app/@services/auth.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  user,
+} from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  firebaseAuth = inject(Auth);
+
+  constructor() {}
+
+  register(
+    email: string,
+    username: string,
+    password: string
+  ): Observable<void> {
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) =>
+      updateProfile(response.user, { displayName: username })
+    );
+
+    return from(promise)
+  }
+}
--- meu-PFC/src/app/app.config.ts
+++ meu-PFC/src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+
+import {provideFirebaseApp, initializeApp} from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 import {
@@ -10,8 +13,21 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAeEsYZCJ6gv6uywN0ErucqVrSpb7G0GXU",
+  authDomain: "meupfc-e9539.firebaseapp.com",
+  projectId: "meupfc-e9539",
+  storageBucket: "meupfc-e9539.firebasestorage.app",
+  messagingSenderId: "599091642741",
+  appId: "1:599091642741:web:d2b4debfddcd21ec89d33c",
+  measurementId: "G-KB5KJJDZ9Z"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
@@ -20,6 +36,6 @@ export const appConfig: ApplicationConfig = {
             theme: {
                 preset: Aura
             }
-        })
+        }),
   ],
 };
