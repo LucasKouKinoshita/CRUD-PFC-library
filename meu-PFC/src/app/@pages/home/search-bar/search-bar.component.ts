242dc5d3c7d3f939d4cdfa35e926893134627470
Lucas Werlang <lucaswewe2806@gmail.com>
2025-05-04 23:24:30 -0300

    Changes on search bar to use the search state service

--- meu-PFC/src/app/@pages/home/search-bar/search-bar.component.ts
+++ meu-PFC/src/app/@pages/home/search-bar/search-bar.component.ts
@@ -1,45 +1,38 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
-import { SearchBarService } from '../../../@services/search-bar.service';
-import { Observable } from 'rxjs';
+import { SearchStateService } from '../../../@services/search-state.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
     InputTextModule,
     FloatLabel,
+    FormsModule,
     InputGroupModule,
     InputGroupAddonModule,
     ButtonModule,
-    DropdownModule,
+    DropdownModule
   ],
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css'],
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
   inputValue: string = '';
-  selectedTag: string = 'title';
-  tccs$: Observable<any[]> | undefined;
+  selectedTag: string = 'Title';
+  tagOptions: string[] = ['Orientator', 'Author', 'Title'];
 
-  tagOptions = [
-    { label: 'Title', value: 'title' },
-    { label: 'Author', value: 'author' },
-    { label: 'Orientator', value: 'orientator' },
-  ];
+  constructor(private searchState: SearchStateService) {}
 
-  constructor(private searchBarService: SearchBarService) {}
-
-  onSearch(): void {
-    this.tccs$ = this.searchBarService.searchTccs(this.selectedTag, this.inputValue);
+  onSearchClick() {
+    this.searchState.setSearchTerm(this.inputValue.trim(), this.selectedTag.toLowerCase());
   }
 }
