908309a20560c5064f29af434ed6e99744a6f69e
Lucas Werlang <lucaswewe2806@gmail.com>
2025-05-04 23:29:08 -0300

    Tryout to make search bar filter the TCCs

--- meu-PFC/src/app/@pages/home/display/display.component.ts
+++ meu-PFC/src/app/@pages/home/display/display.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PfcService } from '../../../@services/pfc.service';
+import { SearchStateService } from '../../../@services/search-state.service';
+import { SearchBarService } from '../../../@services/search-bar.service';
 import { DocumentData } from 'firebase/firestore';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
@@ -14,16 +16,33 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './display.component.css',
 })
 export class DisplayComponent implements OnInit {
-  workList!: (DocumentData | (DocumentData & { id: string; }))[]
+  workList!: (DocumentData & { id?: string })[];
 
-  constructor(private readonly pfcService: PfcService) {}
+  constructor(
+    private readonly pfcService: PfcService,
+    private readonly searchState: SearchStateService,
+    private readonly searchBarService: SearchBarService
+  ) {}
 
   ngOnInit(): void {
-    this.pfcService.getPfcs().subscribe(async data => {
-      this.workList = [];
-      for (const item of data) {
-        this.workList.push(item)
+    // Escuta atualizações do campo de pesquisa
+    this.searchState.searchTerm$.subscribe((search) => {
+      if (search && search.term) {
+        this.searchBarService.searchTccs(search.field, search.term).subscribe(data => {
+          this.workList = data;
+        });
+      } else {
+        this.loadAll();
       }
+    });
+
+    // Carrega tudo inicialmente
+    this.loadAll();
+  }
+
+  loadAll() {
+    this.pfcService.getPfcs().subscribe(data => {
+      this.workList = data;
     });
   }
 
@@ -32,11 +51,11 @@ export class DisplayComponent implements OnInit {
     const url = URL.createObjectURL(blob);
     window.open(url, '_blank');
   }
-  
+
   base64ToBlob(base64: string, contentType: string): Blob {
     const byteCharacters = atob(base64);
     const byteArrays = [];
-  
+
     for (let offset = 0; offset < byteCharacters.length; offset += 512) {
       const slice = byteCharacters.slice(offset, offset + 512);
       const byteNumbers = new Array(slice.length);
@@ -45,7 +64,7 @@ export class DisplayComponent implements OnInit {
       }
       byteArrays.push(new Uint8Array(byteNumbers));
     }
-  
+
     return new Blob(byteArrays, { type: contentType });
   }
 }
