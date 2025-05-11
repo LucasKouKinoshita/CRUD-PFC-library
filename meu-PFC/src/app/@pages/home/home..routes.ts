44f34b2c1b04bcf38deed5b1d233a917d13cbdc1
Filipe Ribeiro <filipe.er99@hotmail.com>
2025-05-11 15:13:23 -0300

    perfil-page

--- meu-PFC/src/app/@pages/home/home..routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const homeRoutes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () =>
-      import('./home/home.component').then((m) => m.HomeComponent),
-  },
-];
